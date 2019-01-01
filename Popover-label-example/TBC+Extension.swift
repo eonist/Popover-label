0bcba549dd73a276b7f9699483d8151113464ff8
Eonist <30n1st@gmail.com>
2019-01-01 16:45:17 +0100

    Minor re-name

--- Popover-label-example/TBC+Extension.swift
+++ Popover-label-example/TBC+Extension.swift
@@ -7,13 +7,13 @@ extension UITabBarController {
     * Creates a popover tag
     */
    func createPopoverTag(){
-      let tagView:PopoverLabel = PopoverLabel.init(text:"♥ 4",style:PopoverLabel.defaultStyle,alignment:.bottom)
-      self.view.addSubview(tagView)
-      tagView.activateAnchor { view in
+      let label:PopoverLabel = .init(text:"♥ 4",style:PopoverLabel.defaultStyle,alignment:.bottom)
+      self.view.addSubview(label)
+      label.activateAnchor { view in
          let offset:CGFloat = -self.tabBar.frame.height - UIApplication.shared.statusBarFrame.height/*To get above the tabBar element*/
          return Constraint.anchor(view, to: self.view, align: .bottomCenter, alignTo: .bottomCenter, offset:.init(x:0,y:offset))
       }
-      tagView.hide()/*Set init state to hidden*/
-      tagView.intro()/*Begins the looping animation*/
+      label.hide()/*Set init state to hidden*/
+      label.intro()/*Begins the looping animation*/
    }
 }
