3c3d0400dd381fc503c84ac4cad614a7ae5d97ec
Eonist <30n1st@gmail.com>
2019-01-01 16:40:33 +0100

    Build framework

---
+++ Carthage/Build/iOS/PopoverLabelLib.framework/Headers/PopoverLabelLib.h
@@ -0,0 +1,19 @@
+//
+//  PopoverLabelLib.h
+//  PopoverLabelLib
+//
+//  Created by Eon on 01/01/2019.
+//  Copyright © 2019 FutureLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PopoverLabelLib.
+FOUNDATION_EXPORT double PopoverLabelLibVersionNumber;
+
+//! Project version string for PopoverLabelLib.
+FOUNDATION_EXPORT const unsigned char PopoverLabelLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopoverLabelLib/PublicHeader.h>
+
+
